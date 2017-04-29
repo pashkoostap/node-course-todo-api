0c16ebbfbbdc939a253418d65f635e5b3d740785
pashkoostap <pashko.ostap@gmail.com>
2017-04-29 13:01:32 +0300

    created tests for POST /todos

--- server/server.js
+++ server/server.js
@@ -17,11 +17,15 @@ app.post('/todos', (req, res) => {
 
   todo.save().then((doc) => {
     res.send(doc);
-  }, (e) => {
-    res.status(400).send(e);
+  }, (err) => {
+    res.status(400).send(err);
   });
 });
 
 app.listen(3000, () => {
   console.log('Started on port 3000');
 });
+
+module.exports = {
+  app
+}
