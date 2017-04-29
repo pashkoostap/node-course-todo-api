6ae04bf6dd6c95d516ae7a5fad9e06156febd642
pashkoostap <pashko.ostap@gmail.com>
2017-04-29 16:54:41 +0300

    implemented DELETE /todos/:id

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 let DB = 'mongodb://ostap:test@ds062339.mlab.com:62339/todo-app';
+let LocalDB = 'mongodb://localhost:27017/todo-app';
 mongoose.connect(DB);
 
 module.exports = {
--- server/server.js
+++ server/server.js
@@ -49,6 +49,22 @@ app.get('/todos/:id', (req, res) => {
     })
 })
 
+app.delete('/todos/:id', (req, res) => {
+  let todoID = req.params.id;
+  if (!ObjectID.isValid(todoID)) {
+    return res.status(404).send();
+  }
+  Todo.findByIdAndRemove(todoID).then(
+    (todo) => {
+      if (!todo) {
+        return res.status(404).send();
+      }
+      res.send({ todo });
+    }).catch((err) => {
+      res.status(404).send();
+    })
+})
+
 app.listen(port, () => {
   console.log(`Started at port ${port}`);
 });
