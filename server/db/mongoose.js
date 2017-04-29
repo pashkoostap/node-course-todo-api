58f9f53cea80f939103e3359a086442d82215b09
pashkoostap <pashko.ostap@gmail.com>
2017-04-29 17:04:40 +0300

    updated DB connection

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,8 +1,13 @@
 const mongoose = require('mongoose');
 
+let DB;
+if (process.env.PORT) {
+  DB = 'mongodb://ostap:test@ds062339.mlab.com:62339/todo-app';
+} else {
+  DB = 'mongodb://localhost:27017/todo-app';
+}
+
 mongoose.Promise = global.Promise;
-let DB = 'mongodb://ostap:test@ds062339.mlab.com:62339/todo-app';
-let LocalDB = 'mongodb://localhost:27017/todo-app';
 mongoose.connect(DB);
 
 module.exports = {
