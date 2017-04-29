cb61a4642572cea3f5895918a35a6b0ee89b4158
pashkoostap <pashko.ostap@gmail.com>
2017-04-29 14:20:56 +0300

    created GET /todos/:id logic

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const { ObjectID } = require('mongodb');
 
 const { mongoose } = require('./db/mongoose');
 const { Todo } = require('./models/todo');
@@ -30,6 +31,22 @@ app.get('/todos', (req, res) => {
       res.status(400).send(err);
     })
 });
+
+app.get('/todos/:id', (req, res) => {
+  let todoID = req.params.id;
+  if (!ObjectID.isValid(todoID)) {
+    return res.status(404).send();
+  }
+  Todo.findById(todoID).then(
+    (todo) => {
+      if (!todo) {
+        return res.status(404).send();
+      }
+      res.send({ todo });
+    }).catch((err) => {
+      res.status(404).send();
+    })
+})
 
 app.listen(3000, () => {
   console.log('Started on port 3000');
